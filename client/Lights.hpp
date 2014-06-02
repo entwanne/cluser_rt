31373110e89ce67e2ac1b04ba28beadab8a63b0f
Antoine Rozo <antoine.rozo@gmail.com>
2014-06-02 21:48:45 +0200

    Shadows

--- client/Lights.hpp
+++ client/Lights.hpp
@@ -1,6 +1,8 @@
 #ifndef _LIGHTS_HPP_
 #define _LIGHTS_HPP_
 
+extern Scene* scene;
+
 class Ambiant : public Light
 {
 public:
@@ -28,6 +30,10 @@ public:
   double intensity_at(const Intersection& inter)
   {
     Point point = inter.ray.point(inter.k);
+    Vector to_obj(point.x - pos.x, point.y - pos.y, point.z - pos.z);
+    Intersection shadow = scene->intersect(Ray(pos, to_obj));
+    if (shadow.object != inter.object)
+      return 0;
     Vector normal = inter.normal();
     Vector to_light(pos.x - point.x, pos.y - point.y, pos.z - point.z);
     double cos_angle = (normal * to_light) / (normal.norm() * to_light.norm());
@@ -51,6 +57,10 @@ public:
   double intensity_at(const Intersection& inter)
   {
     Point point = inter.ray.point(inter.k);
+    Vector to_obj(point.x - pos.x, point.y - pos.y, point.z - pos.z);
+    Intersection shadow = scene->intersect(Ray(pos, to_obj));
+    if (shadow.object != inter.object)
+      return 0;
     Vector normal = inter.normal();
     Vector to_light(pos.x - point.x, pos.y - point.y, pos.z - point.z);
     normal = (1. / normal.norm()) * normal;
