54d0eac4e0fd468b9828f658db654a74c98b839f
Antoine Rozo <antoine.rozo@gmail.com>
2014-06-02 22:23:15 +0200

    Optimize polynom resolution

--- client/Polynome.hpp
+++ client/Polynome.hpp
@@ -1,27 +1,19 @@
 #ifndef _POLYNOME_HPP_
 #define _POLYNOME_HPP_
 
-class NoRootExists
-{};
-
 template < int N >
 class Polynome
 {
 public:
-  Polynome(const std::array< double, N + 1 >& a) : _a(a), _roots(), _i(0)
+  Polynome(const std::array< double, N + 1 >& a) : _a(a), roots(), n_roots(0)
   {}
   void solve()
   {}
-  double next_root()
-  {
-    if (_i >= N)
-      throw NoRootExists();
-    return _roots[_i++];
-  }
+public:
+  std::array< double, N > roots;
+  int n_roots;
 protected:
   std::array< double, N + 1 > _a;
-  std::array< double, N > _roots;
-  int _i;
 };
 
 template < >
@@ -29,27 +21,23 @@ void Polynome< 2 >::solve()
 {
   double delta = SQR(_a[1]) - 4 * _a[0] * _a[2];
   if (delta < 0)
-    throw NoRootExists();
-  _roots[0] = (-_a[1] - sqrt(delta)) / (2 * _a[0]);
-  _roots[1] = (-_a[1] + sqrt(delta)) / (2 * _a[0]);
+    return ;
+  roots[0] = (-_a[1] - sqrt(delta)) / (2 * _a[0]);
+  roots[1] = (-_a[1] + sqrt(delta)) / (2 * _a[0]);
+  n_roots = 2;
 }
 
 template < typename T >
 double polynome_min_positive_root(T& eq)
 {
   double mink = 0;
-  try
+  eq.solve();
+  for (int i = 0; i < eq.n_roots; ++i)
     {
-      eq.solve();
-      while (true)
-  	{
-  	  double k = eq.next_root();
-	  if (LESS_AND_POSITIVE(k, mink))
-  	    mink = k;
-  	}
+      double k = eq.roots[i];
+      if (LESS_AND_POSITIVE(k, mink))
+	mink = k;
     }
-  catch (const NoRootExists&)
-    {}
   return mink;
 }
 
